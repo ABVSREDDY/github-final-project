61e5251dc63434c24abef09bf1abbead523d17cb
ABVSREDDY <abvs.reddy@gmail.com>
2024-08-21 14:42:01 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <ABVSREDDY>
+   # <A B VENKAT SUBBA REDDY>
    # Input:
    # p, principal amount
    # t, time period in years
